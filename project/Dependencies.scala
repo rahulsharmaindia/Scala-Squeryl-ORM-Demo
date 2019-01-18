d8a23521b85dc6307f26a3d4202744c4eb2fcc76
divyanshi1993 <divyanshi.parashar1994@gmail.com>
2019-01-18 17:47:40 +0200

    updated the cglib version to fix an issue with squeryl

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val h2Version = "1.3.166"
   val squeryl = "org.squeryl" %% "squeryl" % squerylVersion % "compile" exclude ("cglib","cglib-nodep") withSources()
 
-  val cglib = "cglib" % "cglib-nodep" % "3.2.10" withSources()
+  val cglib = "cglib" % "cglib-nodep" % "3.2.0" withSources()
   //val postgresql = "org.postgresql" % "postgresql" % postgresqlVersion % "runtime" withSources()
   val h2 = "com.h2database" % "h2" % h2Version withSources()
  // val cglib = "cglib" % "cglib-nodep" % "3.2.10" withSources()
